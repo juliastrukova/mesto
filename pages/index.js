d00fd98051415550aea1034a9f2cb51ef7ed5d9b
juliastrukova <julia.strukova.04@gmail.com>
2022-01-14 18:57:41 +0300

    хедер, галерея, футер, js сердечко

---
+++ pages/index.js
@@ -0,0 +1,90 @@
+// 	buttonHeart
+(function () {
+	var flag = 0;
+	buttonHeart1.onclick = function () {
+		if (flag == 0) { 
+  buttonHeart1.classList.add('element__heart');
+  buttonHeart1.classList.remove('element__heart-active');
+			flag = 1;
+		} else {
+      buttonHeart1.classList.remove('element__heart');
+      buttonHeart1.classList.add('element__heart-active');
+			flag = 0;
+		}
+	}
+})();
+
+(function () {
+	var flag = 0;
+	buttonHeart2.onclick = function () {
+		if (flag == 0) { 
+  buttonHeart2.classList.add('element__heart');
+  buttonHeart2.classList.remove('element__heart-active');
+			flag = 1;
+		} else {
+      buttonHeart2.classList.remove('element__heart');
+      buttonHeart2.classList.add('element__heart-active');
+			flag = 0;
+		}
+	}
+})();
+
+(function () {
+	var flag = 0;
+	buttonHeart3.onclick = function () {
+		if (flag == 0) { 
+  buttonHeart3.classList.add('element__heart');
+  buttonHeart3.classList.remove('element__heart-active');
+			flag = 1;
+		} else {
+      buttonHeart3.classList.remove('element__heart');
+      buttonHeart3.classList.add('element__heart-active');
+			flag = 0;
+		}
+	}
+})();
+
+(function () {
+	var flag = 0;
+	buttonHeart4.onclick = function () {
+		if (flag == 0) { 
+  buttonHeart4.classList.add('element__heart');
+  buttonHeart4.classList.remove('element__heart-active');
+			flag = 1;
+		} else {
+      buttonHeart4.classList.remove('element__heart');
+      buttonHeart4.classList.add('element__heart-active');
+			flag = 0;
+		}
+	}
+})();
+
+(function () {
+	var flag = 0;
+	buttonHeart5.onclick = function () {
+		if (flag == 0) { 
+  buttonHeart5.classList.add('element__heart');
+  buttonHeart5.classList.remove('element__heart-active');
+			flag = 1;
+		} else {
+      buttonHeart5.classList.remove('element__heart');
+      buttonHeart5.classList.add('element__heart-active');
+			flag = 0;
+		}
+	}
+})();
+
+(function () {
+	var flag = 0;
+	buttonHeart6.onclick = function () {
+		if (flag == 0) { 
+  buttonHeart6.classList.add('element__heart');
+  buttonHeart6.classList.remove('element__heart-active');
+			flag = 1;
+		} else {
+      buttonHeart6.classList.remove('element__heart');
+      buttonHeart6.classList.add('element__heart-active');
+			flag = 0;
+		}
+	}
+})();
