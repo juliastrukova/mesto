1e3a9cdd0d13e9d17c32490665f8690fbbd537da
juliastrukova <julia.strukova.04@gmail.com>
2022-01-18 14:36:33 +0300

    правки после 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 // объявление для открытия/закрытия формы
 const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__button-close');
-const savePopupButton = document.querySelector('.popup__button-save');
-// объявления дл ясохранения формы из попапа в профиль
+// объявления для сохранения формы из попапа в профиль
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.getElementById('name');
 const descriptionInput = document.getElementById('description');
+const profile = document.querySelector('.profile');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 // функции открытия/закрытия формы
-function openPopup(event) {
-  event.preventDefault();
+function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
 }
 
 function closePopup() {
@@ -23,12 +26,8 @@ closePopupButton.addEventListener('click', closePopup);
 // функция сохранения формы в профиль
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  const userName = nameInput.value;
-  const userDescription = descriptionInput.value;
-  const profileName = document.querySelector('.profile__name');
-  const profileDescription = document.querySelector('.profile__description');
-  profileName.textContent = userName;
-  profileDescription.textContent = userDescription;
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
   closePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler);
