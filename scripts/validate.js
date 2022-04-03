e14f5c89f79049299c66d16278e3ace5b4e15c1e
juliastrukova <julia.strukova.04@gmail.com>
2022-04-03 20:48:00 +0300

    пр 6

--- scripts/validate.js
+++ scripts/validate.js
@@ -25,10 +25,8 @@ const validationList = {
   const checkInputValidity = (formElement, inputElement) => {
     if (!inputElement.validity.valid) {
       showInputError(formElement, inputElement, inputElement.validationMessage);
-
     } else {
       hideInputError(formElement, inputElement);
-
     }
   };
 
